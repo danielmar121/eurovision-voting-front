d9f514426f7c9aa49bcabeb350d582480254268e
Daniel <53561824+danielmar121@users.noreply.github.com>
2024-05-02 19:45:22 +0300

    Fix Snackbar in mobiles

--- src/components/shared/AppSnackbar.js
+++ src/components/shared/AppSnackbar.js
@@ -1,24 +1,11 @@
-import { makeStyles } from "@material-ui/core/styles";
 import { SnackbarProvider } from "notistack";
 import PropTypes from "prop-types";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    "& > * + *": {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
-
 export default function AppSnackbar({ children }) {
-  const classes = useStyles();
-
   return (
     <SnackbarProvider
-      className={classes.root}
       maxSnack={6}
-      autoHideDuration={4000}
+      autoHideDuration={5000}
       anchorOrigin={{
         vertical: "bottom",
         horizontal: "center",
